Fix functions for gradient accumulation

ALBERT-master/gradient_accumulation.py
@@ -14,7 +14,7 @@ def use_gradient_accumulation(grads, tvars, optimizer, gradient_accmulation_mult
                                     initializer=tf.zeros_initializer,
                                     trainable=False,
                                     dtype=tf.float32,
-                                    collections=[tf.GraphKeys.LOCAL_VARIABLES]) for i, tv in enumerate(tvars)]
+                                    collections=[tf.GraphKeys.LOCAL_VARIABLES], aggregation=tf.VariableAggregation.MEAN) for i, tv in enumerate(tvars)]
     sum_ops = []
     unused_variable_in_batch = []
 
@@ -42,7 +42,8 @@ def use_gradient_accumulation(grads, tvars, optimizer, gradient_accmulation_mult
 
     train_op = tf.cond(tf.math.equal(global_step % gradient_accmulation_multiplier, 0),
                     lambda: apply_accumulated_gradients(sum_gradient),
-                    lambda: optimizer.apply_gradients(zip([None for _ in grads], tvars), global_step=global_step))
+                    tf.no_op)
+                    #lambda: optimizer.apply_gradients(zip([None for _ in grads], tvars), global_step=global_step))
 
     # reset accumulation when necessary
     def reset():
@@ -71,6 +72,7 @@ class GradientAccumulationHook(tf.train.SessionRunHook):
     """
     Puts a certain tf.Variable to 1 once every certain steps.
     """
+
 
     def __init__(self, frequency, variable):
         self._step = 0

ALBERT-master/optimization.py
@@ -25,12 +25,15 @@ from six.moves import zip
 import tensorflow.compat.v1 as tf
 from tensorflow.contrib import tpu as contrib_tpu
 from adamw import AdamWeightDecayOptimizer
+from gradient_accumulation import use_gradient_accumulation
 
 
 def create_optimizer(loss, init_lr, num_train_steps, num_warmup_steps, use_tpu,
-                     optimizer="adamw", poly_power=1.0, start_warmup_step=0):
+                     optimizer="adamw", poly_power=1.0, start_warmup_step=0, gradient_accumulation_multiplier=1, do_update=None):
   """Creates an optimizer training op."""
   global_step = tf.train.get_or_create_global_step()
+
+  use_accumulation = gradient_accumulation_multiplier > 1 and do_update is not None
 
   learning_rate = tf.constant(value=init_lr, shape=[], dtype=tf.float32)
 
@@ -101,16 +104,20 @@ def create_optimizer(loss, init_lr, num_train_steps, num_warmup_steps, use_tpu,
   # This is how the model was pre-trained.
   (grads, _) = tf.clip_by_global_norm(grads, clip_norm=1.0)
 
+  if use_accumulation:
+    train_op = use_gradient_accumulation(grads, tvars, optimizer, gradient_accumulation_multiplier, global_step, do_update)
+  else:
+    train_op = optimizer.apply_gradients(
+        list(zip(grads, tvars)), global_step=global_step)
 
-  train_op = optimizer.apply_gradients(
-      list(zip(grads, tvars)), global_step=global_step)
+      # Normally the global step update is done inside of `apply_gradients`.
+    # However, neither `AdamWeightDecayOptimizer` nor `LAMBOptimizer` do this.
+    # But if you use a different optimizer, you should probably take this line
+    # out.
+    new_global_step = global_step + 1
+    train_op = tf.group(train_op, [global_step.assign(new_global_step)])
 
-  # Normally the global step update is done inside of `apply_gradients`.
-  # However, neither `AdamWeightDecayOptimizer` nor `LAMBOptimizer` do this.
-  # But if you use a different optimizer, you should probably take this line
-  # out.
-  new_global_step = global_step + 1
-  train_op = tf.group(train_op, [global_step.assign(new_global_step)])
+  
   return train_op
 
 '''

run-albert-hate.py
@@ -271,7 +271,7 @@ def model_fn_builder(regression=args.regression):
             train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
             accumulation_hook = None
         elif args.optimizer == 'adamw' or args.optimizer == 'lamb':
-            do_update = tf.get_variable('do_update', shape=(), dtype=tf.int32, initializer=tf.constant_initializer())
+            do_update = tf.get_variable('do_update', shape=(), dtype=tf.float32, initializer=tf.constant_initializer())
             accumulation_hook = GradientAccumulationHook(args.accumulation_steps, do_update)
             train_op = optimization.create_optimizer(
             loss, args.learning_rate, ITERATIONS, args.warmup_steps,
@@ -286,9 +286,8 @@ def model_fn_builder(regression=args.regression):
                     tf.summary.histogram(var.op.name + '/gradients', grad)
         for var in tf.trainable_variables():
             tf.summary.histogram(var.name, var)
-        logging_hook = tf.train.LoggingTensorHook({"accuracy": "acc"},                                                          
-                                    every_n_iter=args.accumulation_steps)
-        return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op, training_hooks=[logging_hook, accumulation_hook])
+        #logging_hook = tf.train.LoggingTensorHook({"accuracy": "acc"}, every_n_iter=args.accumulation_steps)
+        return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op, training_hooks=[accumulation_hook])
     return my_model_fn
 
 
@@ -420,7 +419,7 @@ if __name__ == "__main__":
             classifier.evaluate(input_fn=lambda:eval_input_fn(2, test=True), steps=None)
         else:
             early_stopping = tf.estimator.experimental.stop_if_no_decrease_hook(classifier, metric_name="loss", max_steps_without_decrease=10000, min_steps=100)
-            train_spec = tf.estimator.TrainSpec(input_fn=lambda: train_input_fn(BATCH_SIZE), max_steps=ITERATIONS, hooks=[early_stopping])
+            train_spec = tf.estimator.TrainSpec(input_fn=lambda: train_input_fn(args.mini_batch_size), max_steps=ITERATIONS, hooks=[early_stopping])
             eval_spec = tf.estimator.EvalSpec(input_fn=lambda:eval_input_fn(BATCH_SIZE), steps=None)
 
             tf.estimator.train_and_evaluate(classifier, train_spec, eval_spec)