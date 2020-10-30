4617f941fad0e71e299754570aec750d7c019b3d
PeterBul <pc.bulukin@gmail.com>
2020-10-30 16:00:43 +0100

    Added preprocessing test

---
+++ preprocessing_test.py
@@ -0,0 +1,16 @@
+import unittest
+import preprocessing
+import pandas as pd
+
+class TestPreprocessing(unittest.TestCase):
+
+  def test_get_train_dev_test_dataframes(self):
+    processor = preprocessing.FountaConvProcessor('data')
+    train, dev, test = processor.get_train_dev_test_dataframes()
+    self.assertIsInstance(train, pd.DataFrame)
+    self.assertIsInstance(dev, pd.DataFrame)
+    self.assertIsInstance(test, pd.DataFrame)
+  
+
+if __name__ == "__main__":
+  unittest.main()
